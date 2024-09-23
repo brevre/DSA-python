9360c5077a12a72018c25556c46d8d606b5671c9
brevre <136987451+brevre@users.noreply.github.com>
2024-09-23 15:54:53 +0300

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,8 @@
+name = input("please input your name:")
+age = input("please input your age:")
+location = input("please input your location:")
+print(f"Hello{name},you are{age} years old and live in {location}")
+
+name = Happy
+age= 25
+location = newyork
